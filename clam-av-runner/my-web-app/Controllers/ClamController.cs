292b2be55c3b997db7e45d818d5cb0f53dd3c6d3
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-07-29 01:17:20 +0530

    Completed

---
+++ clam-av-runner/my-web-app/Controllers/ClamController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using nClam;
+using System.Web;
+
+namespace my_web_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClamController : ControllerBase
+    {
+        [HttpPost("files/{name}/scan")]
+        public async Task<Result> Scan([FromRoute]String name)
+        {
+            try
+            {
+                name = HttpUtility.UrlDecode(name);
+                var clam = new ClamClient("localhost", 3310);
+
+                var sw = Stopwatch.StartNew();
+
+                var scanResult = await clam.ScanFileOnServerAsync(name);  //any file you would like!
+
+                long ticks = sw.ElapsedTicks;
+                var time = sw.ElapsedMilliseconds;
+
+                switch (scanResult.Result)
+                {
+                    case ClamScanResults.Clean:
+                        return new Result() { FileName = name, IsCompleted = true, IsClean = true, TimeTakenInMs = time };
+                    case ClamScanResults.VirusDetected:
+                        return new Result() { FileName = name, IsCompleted = true, IsClean = false, TimeTakenInMs = time };
+                    case ClamScanResults.Error:
+                    default:
+                        return new Result() { FileName = name, IsCompleted = false };
+                }
+            }
+            catch(Exception e)
+            {
+                return new Result() { FileName = name, IsCompleted = false };
+            }
+            
+        }
+    }
+
+    public class Result
+    {
+        public bool IsClean { get; set; }
+        public bool IsCompleted { get; set; }
+        public long TimeTakenInMs { get; set; }
+
+        public String FileName { get; set; }
+    }
+}
