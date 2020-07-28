e4a40c988e25b2ed59b52195209510af09d706ab
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-07-29 01:33:16 +0530

    Completed

---
+++ clam-av-runner/push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker tag my-clam nachiappan/my-clam
+docker push nachiappan/my-clam
+
+
---
+++ clam-av-runner/run.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+docker create --name my-clam -p 5050:5050 nachiappan/my-clam
+docker cp 
